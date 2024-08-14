5624d32ad144a242705838a37d2578bc957e4233
Teppei Yoshida New Mac <teppei@njc.co.jp>
2024-08-14 18:37:01 +0900

    first commit

--- src/app/books/[[...keyword]]/page.js
+++ src/app/books/[[...keyword]]/page.js
@@ -1,73 +1,80 @@
-'use client'; // クライアントコンポーネントとしてマーク
+"use client"; // クライアントコンポーネントとしてマーク
 
-import { useState, useEffect } from 'react';
-import LinkedBookDetails from '@/components/LinkedBookDetails';
-import PaginationComponent from '@/components/PaginationComponent';
-import { getBooksByKeyword } from '@lib/getter';
+import { useState, useEffect } from "react";
+import LinkedBookDetails from "@/components/LinkedBookDetails";
+import PaginationComponent from "@/components/PaginationComponent";
+import { getBooksByKeyword } from "@lib/getter";
 
 // ルートパラメーターparamsを取得
 export default function BookResult({ params }) {
-    const keyword = params.keyword || ''; // URLからキーワードを取得し、デフォルト値を設定
-    const [books, setBooks] = useState([]); // 書籍データを格納する状態
-    const [page, setPage] = useState(1); // 現在のページ番号を格納する状態
-    const [error, setError] = useState(null); // エラーメッセージを格納する状態
-    const booksPerPage = 10; // 1ページあたりの書籍数
+  const keyword = params.keyword || ""; // URLからキーワードを取得し、デフォルト値を設定
+  const [books, setBooks] = useState([]); // 書籍データを格納する状態
+  const [page, setPage] = useState(1); // 現在のページ番号を格納する状態
+  const [error, setError] = useState(null); // エラーメッセージを格納する状態
+  const booksPerPage = 10; // 1ページあたりの書籍数
 
-    useEffect(() => {
-        const fetchBooks = async () => {
-            if (!keyword) {
-                setError('キーワードが指定されていません。');
-                return;
-            }
-    
-            // keywordを文字列に変換
-            const searchKeyword = String(keyword);
-            
-            try {
-                const fetchedBooks = await getBooksByKeyword(searchKeyword, page, booksPerPage); // 書籍データを取得
-                console.log('Fetched Books:', fetchedBooks); // 追加
-                setBooks(fetchedBooks);
-                setError(null);
-            } catch (error) {
-                console.error("書籍データの取得中にエラーが発生しました:", error);
-                setError('書籍情報の取得に失敗しました。もう一度お試しください。');
-            }
-        };
-    
-        fetchBooks();
-    }, [keyword, page]);
-    
-    // ページ変更時に呼び出される関数
-    const handleChange = (event, value) => {
-        setPage(value); // 現在のページ番号を更新
+  useEffect(() => {
+    const fetchBooks = async () => {
+      if (!keyword) {
+        setError("キーワードが指定されていません。");
+        return;
+      }
+
+      // keywordを文字列に変換
+      const searchKeyword = String(keyword);
+
+      try {
+        const fetchedBooks = await getBooksByKeyword(
+          searchKeyword,
+          page,
+          booksPerPage
+        ); // 書籍データを取得
+        console.log("Fetched Books:", fetchedBooks); // 追加
+        setBooks(fetchedBooks);
+        setError(null);
+      } catch (error) {
+        console.error("書籍データの取得中にエラーが発生しました:", error);
+        setError("書籍情報の取得に失敗しました。もう一度お試しください。");
+      }
     };
 
-    // 現在のページに表示する書籍データを計算
-    const paginatedBooks = books.slice((page - 1) * booksPerPage, page * booksPerPage);
+    fetchBooks();
+  }, [keyword, page]);
 
-    return (
-        <div>
-            {error ? (
-                <p>{error}</p> // エラーメッセージがある場合に表示
-            ) : (
-                <>
-                    {/* 現在のページに表示する書籍データをリスト表示 */}
-                    {books.length > 0 ? (
-                    paginatedBooks.map((b, i) => (
-                        <LinkedBookDetails book={b} index={i + 1} key={b.id} />
-                    ))
-                    ) : (
-                        <p>書籍情報が見つかりませんでした。</p>
-                    )}
+  // ページ変更時に呼び出される関数
+  const handleChange = (event, value) => {
+    setPage(value); // 現在のページ番号を更新
+  };
 
-                    {/* ページネーションコンポーネント */}
-                    <PaginationComponent
-                        page={page} // 現在のページ番号
-                        count={Math.ceil(books.length / booksPerPage)} // 総ページ数
-                        onChange={handleChange} // ページ変更時に呼び出される関数
-                    />
-                </>
-            )}
-        </div>
-    );
+  // 現在のページに表示する書籍データを計算
+  const paginatedBooks = books.slice(
+    (page - 1) * booksPerPage,
+    page * booksPerPage
+  );
+
+  return (
+    <div>
+      {error ? (
+        <p>{error}</p> // エラーメッセージがある場合に表示
+      ) : (
+        <>
+          {/* 現在のページに表示する書籍データをリスト表示 */}
+          {books.length > 0 ? (
+            paginatedBooks.map((b, i) => (
+              <LinkedBookDetails book={b} index={i + 1} key={b.id} />
+            ))
+          ) : (
+            <p>書籍情報が見つかりませんでした。</p>
+          )}
+
+          {/* ページネーションコンポーネント */}
+          <PaginationComponent
+            page={page} // 現在のページ番号
+            count={Math.ceil(books.length / booksPerPage)} // 総ページ数
+            onChange={handleChange} // ページ変更時に呼び出される関数
+          />
+        </>
+      )}
+    </div>
+  );
 }
--- src/app/edit/[id]/page.js
+++ src/app/edit/[id]/page.js
@@ -1,26 +1,45 @@
+"use client";
+
 import BookDetails from "@/components/BookDetails";
-import FormEdit from '@/components/FormEdit';
+import FormEdit from "@/components/FormEdit";
 import { getBookById, getReviewById } from "@lib/getter";
+import { useState, useEffect } from "react";
 
-export default async function EditPage({ params }) {
-    const book = await getBookById(params.id);
-    const review = await getReviewById(params.id);
+export default function EditPage({ params }) {
+  const [book, setBook] = useState({});
+  const [review, setReview] = useState({});
+  // ログイン状態を監視し、ログインしていない場合はモーダルを表示
 
-    if (!book) {
-        return <p>Book not found.</p>; // 書籍が見つからない場合のメッセージ
-    }
+  useEffect(() => {
+    const fetchBooks = async () => {
+      if (!params.id) {
+        return;
+      }
 
+      let book_byid = await getBookById(params.id);
+      console.log("book_byid", book_byid);
+      let review_byid = {}; //await getReviewById(params.id);
+      setBook(book_byid);
+      setReview(review_byid);
+    };
 
-    //「YYYY-MM-DD」形式の日付を生成
-    const read = (review?.read || new Date()).toLocaleDateString('sv-SE');
+    fetchBooks();
+  }, [params.id]);
 
-    return (
-        <div id="form">
-            <BookDetails book={book} />
-            <hr />
+  if (!book) {
+    return <p>Book not found.</p>; // 書籍が見つからない場合のメッセージ
+  }
 
-            {/*編集フォームを生成 */}
-            <FormEdit src={{ id: book.id, read, memo: review?.memo}} />
-        </div>
-    );
+  //「YYYY-MM-DD」形式の日付を生成
+  const read = (review?.read || new Date()).toLocaleDateString("sv-SE");
+
+  return (
+    <div id="form">
+      <BookDetails book={book} />
+      <hr />
+
+      {/*編集フォームを生成 */}
+      <FormEdit src={{ id: book.id, read, memo: review?.memo }} />
+    </div>
+  );
 }
